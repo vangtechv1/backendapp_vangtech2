4955bf3d0a22b396daee72945248f76c8c977c07
typicalsleepingboy <mpluto772@gmail.com>
2024-11-25 21:16:04 +0700

    testing lagi

--- index.js
+++ index.js
@@ -7,26 +7,8 @@ const cors = require('cors');
 
 const app = express();
 connectDB();
-app.use(express.json());
-
-app.use(cors({
-    origin: function(origin, callback){
-        if(!origin) return callback(null, true);
-        
-        const allowedOrigins = [
-            'https://backendapp-vangtech2.vercel.app', 
-            'http://localhost:3000'
-        ];
-        
-        if (allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
     res.send({
