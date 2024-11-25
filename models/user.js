58ea4cf7da65822f5a108d65bbfe759087d54bb4
typicalsleepingboy <mpluto772@gmail.com>
2024-11-25 21:57:43 +0700

    add name in model db

--- models/user.js
+++ models/user.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
 
-const UserSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
     email: {
         type: String,
         required: true,
         unique: true,
-        trim: true
     },
     password: {
         type: String,
-        required: true
-    }
-}, { timestamps: true });
+        required: true,
+    },
+});
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model('User', userSchema);
